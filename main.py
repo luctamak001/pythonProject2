28940fc0f48bff55acde201f949610161fab159d
Makenley Luctamar <luctamak001@warren.k12.in.us>
2024-10-28 07:18:10 -0400

    update

---
+++ main.py
@@ -0,0 +1,15 @@
+# Todo: write something in the
+#  input function
+#  instructing the user to enter their age.
+age = int(input("Enter your age"))
+
+# TODO: Use a relational operator to evaluate is_eligible
+#  YOU are are eligible if your age is greater than or equal
+#  to 18
+
+
+is_eligible = age >= 18
+
+
+# TODO: print the result
+print("You are eligible to vote: ", is_eligible)
